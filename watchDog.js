ca4ee61b6458b89aa0a951a951ae34eb6b0288e2
skipZero <skip@zerosquadron.com>
2016-11-11 02:10:49 -0800

    encoding

--- watchDog.js
+++ watchDog.js
@@ -6,6 +6,8 @@ const gpio = require('wpi-gpio');
 gpio.BCM_GPIO = true;
 
 http.get('http://10.0.0.70', (res, err) => {
+  res.setEncoding('utf8');
+
   res.on('err', (err) => {
     console.log(err);
   });
@@ -13,6 +15,7 @@ http.get('http://10.0.0.70', (res, err) => {
   res.on('data', (data) => {
     let status = data.statusCode;
     if (status === 200) {
+
       console.log('Weeeeee.....')
     }
   })

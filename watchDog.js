bd858a0076e6a2acf47876542baa7089bd2dca17
skipZero <skip@zerosquadron.com>
2016-11-11 01:07:47 -0800

    silly me

--- watchDog.js
+++ watchDog.js
@@ -4,11 +4,11 @@ const gpio = require('wpi-gpio');
 gpio.BCM_GPIO = true;
 
 http.get('http://10.0.0.70', (res, err) => {
-  res.on(err, (err) => {
+  res.on('err', (err) => {
     console.log(err);
   });
 
-  res.on(data, (data) => {
+  res.on('data', (data) => {
     console.log(res);
   })
   console.info(gpio);

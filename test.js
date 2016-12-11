ddb6b07f76ea167aa4b036c01ef3c8c02ce9e285
pi-pi-pi <skip@zerosquadron.com>
2016-12-11 03:06:23 -0800

    fixed script

--- test.js
+++ test.js
@@ -1,50 +1,37 @@
+#!/usr/bin/env node
 'use strict';
 const http = require('http');
 const cmd = require('node-cmd');
-const time = 5000;
+const stationIP = 'http://10.0.0.35';
+const gpioer = 'sudo python /home/pi/node-watchdog/gpio.py';
+const sec = 1000;
 
-const restart = () => {
-  cmd.run('python ../gpio.py on');
-  console.log('fired on!')
-}
-cmd.run('python ../gpio.py off');
-setTimeout(restart(), time);
+const cycleOn = () => {
+  setTimeout(() => {
+    cmd.run(`${gpioer} on`);
+    console.info('set to on...');
+  }, sec);
+};
 
+const cycleOff = () => {
+  cmd.run(`${gpioer} off`);
+  cycleOn();
+  console.info('set pin to off...');
+};
 
+const areYouAwake = () => {
+  http.get(stationIP, (res) => {
+    const code = res.statusCode;
+    res.setEncoding('utf8');
+    if (code === 200) {
+      console.info(`Station up at ${new Date()}`);
+      return;
+    }
+  })
+  .on('error', (err) => {
+    cycleOff();
+    console.log(`reset station, pin ${pin} @ ${new Date()} \n\n ${err}`);
+  });
+};
 
-
-
-
-//
-// const stationIP = 'http://10.0.0.35';
-// const gpioer = 'sudo python /home/pi/gpio.py';
-//
-// const sec = 0.5;
-// const secTimer = sec * 1000;
-//
-// const cycleOn = () => {
-//   setTimeout(() => {
-//     cmd.run(`${gpioer} on`);
-//     console.info('set to on...');
-//   }, secTimer);
-// };
-//
-// const cycleOff = () => {
-//   cmd.run(`${gpioer} off`);
-//   cycleOn();
-//   console.info('set pin to off...');
-// };
-//
-// const areYouAwake = () => {
-//   http.get(stationIP, (res) => {
-//     const code = res.statusCode;
-//     res.setEncoding('utf8');
-//     if (code === 200) {
-//       console.info(`Station up at ${new Date()}`);
-//       return;
-//     }
-//   }).on('error', (err) => {
-//     cycleOff();
-//     console.log(`reset station, pin ${pin} @ ${new Date()}`);
-//   });
-// };
+areYouAwake();
